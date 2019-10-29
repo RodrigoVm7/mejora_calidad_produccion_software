fe122325a4fdb050824df7379142f08e9b8a1ece
karloscs7 <51274782+karloscs7@users.noreply.github.com>
2019-10-29 15:22:09 -0400

    migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->string('rut')->unique();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
